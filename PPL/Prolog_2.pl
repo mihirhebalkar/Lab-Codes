8cb8547f6163e29a11f28e99ecd6cf1a223962d6
Mihir Hebalkar <138997590+mihirhebalkar@users.noreply.github.com>
2023-08-22 12:34:21 +0530

    Update Prolog_2.pl

--- PPL/Prolog_2.pl
+++ PPL/Prolog_2.pl
@@ -7,9 +7,10 @@ symptom(a,headache).
 symptom(a,sore_throat).
 
 symptom(b,rash).
-symptom(b,flu).
+symptom(b,sneezing).
 symptom(b,fever).
 symptom(b,body_ache).
+symptom(b,rash).
 
 symptom(c,chills).
 symptom(c,fever).
@@ -25,6 +26,8 @@ symptom(d,loss_of_taste).
 
 disease(X,corona):-symptom(X,fever),symptom(X,cough),symptom(X,loss_of_taste),!.
 
-disease(X,common_cold):-symptom(X,headache),symptom(X,sneezing),symptom(X,sore_throat),!.
+disease(X,cold):-symptom(X,headache),symptom(X,sneezing),symptom(X,sore_throat),!.
 
 disease(X,flu):-symptom(X,chills),symptom(X,fever),symptom(X,headache),!.
+
+disease(X,allergy):-symptom(X,sneezing),symptom(X,rash),!.
