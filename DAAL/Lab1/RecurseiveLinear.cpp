4047f05a600cd3726f8c67044cb1aef2baaa0905
Mihir Hebalkar <mihirhebalkar07@gmail.com>
2024-09-04 09:23:35 +0530

    Create RecurseiveLinear.cpp

---
+++ DAAL/Lab1/RecurseiveLinear.cpp
@@ -0,0 +1,28 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int recLinear(int arr[], int cur, int n, int key){
+    
+    if(cur>n){
+        return -1;
+    }
+    else if(arr[cur]==key) {
+        return cur;
+    }
+    return recLinear(arr, cur+1, n, key); 
+}
+
+int main() {
+    int arr[5]= {1,4,8,3,2};
+    
+    int n=sizeof(arr)/sizeof(arr[0]);
+    int z=recLinear(arr,0,n,0);
+    
+    if(z==-1){
+        cout<<"Element not Present in array";
+    }
+    else{
+        cout<<"index = "<<z;
+    }
+}
