5db6c7ddc79cc14828eb2312a3caaa229babb8c3
Mihir Hebalkar <mihirhebalkar07@gmail.com>
2024-10-24 10:03:03 +0530

    Update FractionalKnapsack.cpp

--- DAAL/Lab5/FractionalKnapsack.cpp
+++ DAAL/Lab5/FractionalKnapsack.cpp
@@ -1,92 +1,49 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
+#include<bits/stdc++.h>
 
 using namespace std;
 
-bool compare(pair<float, int> p1, pair<float, int> p2)
-{
-    return p1.first > p2.first; 
-}
+struct Item{
+    int value;
+    int weight;
+};
 
-float fractional_knapsack(vector<int> weights, vector<int> values, int capacity)
-{
-    int len = weights.size();
-    float total_value = 0;  
-
-    vector<pair<float, int>> ratio(len);  
-
-
-    for(int i = 0; i < len; i++)
-        ratio[i] = make_pair(float(values[i]) / weights[i], i);  
-
-    sort(ratio.begin(), ratio.end(), compare);
-
-
-    for(int i = 0; i < len; i++)
-    {
-        if(capacity == 0)
-            break;
-        
-        int index = ratio[i].second;
-
-        if(weights[index] <= capacity)
-        {
-            capacity -= weights[index];
-            total_value += values[index];
+class S {
+    
+    public:
+        bool static comp(Item a, Item b){
+            double r1 = (double) a.value/ (double) a.weight;
+            double r2 = (double) b.value/(double) b.weight;
+            
+            return r1>r2;
         }
-        else
-        {
-            total_value += values[index] * (float(capacity) / weights[index]);
-            capacity = 0;  // Knapsack is now full
-        }
-    }
-    return total_value;
-}
-
-int main()
-{
-    cout << "Enter the weights of the items (end with -1):" << endl;
-    vector<int> weights;
-
-    while(true)
-    {
-        int weight;
-        cin >> weight;
-
-        if(weight == -1)
-            break;
-
-        weights.push_back(weight);
-    }
-
-    cout << "Enter the profits of each item (end with -1):" << endl;
-    vector<int> values;
-
-    while(true)
-    {
-        int value;
-        cin >> value;
-
-        if(value == -1)
-            break;
         
-        values.push_back(value);
-    }
-
-    cout << "\nRatios (value/weight) of items: ";
-    for(size_t i = 0; i < weights.size(); i++)
-    {
-        float temp = float(values[i]) / weights[i];
-        cout << temp << " ";
-    }
-    cout << endl;
-
-    cout << "Enter the capacity of the knapsack: ";
-    int capacity;
-    cin >> capacity;
-
-    cout << "The maximum value possible based on current list is: " << fractional_knapsack(weights, values, capacity) << endl;
-
-    return 0;
+        
+        double fk(int W, Item arr[], int n){
+            sort(arr, arr+n, comp);
+            
+            int curWeight = 0;
+            double final = 0.0;
+            
+            for(int i=0; i<n; i++){
+                if(curWeight + arr[i].weight<=W){
+                    curWeight += arr[i].weight;
+                    final += arr[i].value;
+                }
+                else{
+                    int remain = W - curWeight;
+                    final += (double) arr[i].value * (double) remain/ (double) arr[i].weight;
+                    break;
+                }
+            }
+            return final;
+        }
+    
+};
+int main() {
+   int n = 3, weight = 50;
+   Item arr[n] = { {100,20},{60,10},{120,30} };
+   S obj;
+   double ans = obj.fk(weight, arr, n);
+   cout << "The maximum value is " << setprecision(2) << fixed << ans;
+   return 0;
 }
