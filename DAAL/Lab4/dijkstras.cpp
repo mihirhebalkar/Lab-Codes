73ca3ce4764a48686a6ffb10276a2cca8712fa2d
Mihir Hebalkar <mihirhebalkar07@gmail.com>
2024-10-24 13:27:44 +0530

    Update dijkstras.cpp

--- DAAL/Lab4/dijkstras.cpp
+++ DAAL/Lab4/dijkstras.cpp
@@ -1,43 +1,44 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<int> dijkstra(int V, vector<vector<int>> adj[], int S) {
-        set<pair<int,int>> st; 
-        vector<int> dist(V, 1e9); 
-        
-        st.insert({0, S}); 
-        dist[S] = 0;
-        
-        while(!st.empty()) {
-            auto it = *(st.begin()); 
-            int node = it.second; 
-            int dis = it.first; 
-            st.erase(it); 
-            
-            for(auto it : adj[node]) {
-                int adjNode = it[0]; 
-                int edgW = it[1]; 
-                
-                if(dis + edgW < dist[adjNode]) {
-                    if(dist[adjNode] != 1e9) 
-                        st.erase({dist[adjNode], adjNode}); 
-                    
-                    dist[adjNode] = dis + edgW; 
-                    st.insert({dist[adjNode], adjNode}); 
-                 }
+    vector<int> dijkstra(int V, vector<vector<int>> adj[], int S)
+    {
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        vector<int> distTo(V, INT_MAX);
+        distTo[S] = 0;
+        pq.push({0, S});
+
+        while (!pq.empty())
+        {
+            int node = pq.top().second;
+            int dis = pq.top().first;
+            pq.pop();
+
+            for (auto it : adj[node])
+            {
+                int v = it[0];
+                int w = it[1];
+                if (dis + w < distTo[v])
+                {
+                    distTo[v] = dis + w;
+                    pq.push({dis + w, v});
+                }
             }
         }
-        return dist; 
+        return distTo;
     }
 };
 
-int main() {
+int main()
+{
     int V = 3, E = 3, S = 2;
     vector<vector<int>> adj[V];
+    vector<vector<int>> edges;
     vector<int> v1{1, 1}, v2{2, 6}, v3{2, 3}, v4{0, 1}, v5{1, 3}, v6{0, 6};
-    
+    int i = 0;
     adj[0].push_back(v1);
     adj[0].push_back(v2);
     adj[1].push_back(v3);
@@ -48,7 +49,8 @@ int main() {
     Solution obj;
     vector<int> res = obj.dijkstra(V, adj, S);
 
-    for (int i = 0; i < V; i++) {
+    for (int i = 0; i < V; i++)
+    {
         cout << res[i] << " ";
     }
     cout << endl;
