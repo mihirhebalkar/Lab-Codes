636473ea7433e181208b149d3d13ceced969b656
Mihir Hebalkar <mihirhebalkar07@gmail.com>
2024-11-14 09:50:10 +0530

    Updated code to print out included elements.

--- DAAL/Lab5/FractionalKnapsack.cpp
+++ DAAL/Lab5/FractionalKnapsack.cpp
@@ -1,49 +1,77 @@
+#include<iostream>
 #include<bits/stdc++.h>
 
 using namespace std;
 
-struct Item{
-    int value;
-    int weight;
+struct Item {
+	int value;
+	int weight;
 };
 
-class S {
-    
-    public:
-        bool static comp(Item a, Item b){
-            double r1 = (double) a.value/ (double) a.weight;
-            double r2 = (double) b.value/(double) b.weight;
-            
-            return r1>r2;
-        }
-        
-        
-        double fk(int W, Item arr[], int n){
-            sort(arr, arr+n, comp);
-            
-            int curWeight = 0;
-            double final = 0.0;
-            
-            for(int i=0; i<n; i++){
-                if(curWeight + arr[i].weight<=W){
-                    curWeight += arr[i].weight;
-                    final += arr[i].value;
-                }
-                else{
-                    int remain = W - curWeight;
-                    final += (double) arr[i].value * (double) remain/ (double) arr[i].weight;
-                    break;
-                }
+class Solution {
+	public:
+		bool static comp(pair<Item, int>a, pair<Item, int>b){ //to help in sorting
+			double r1 = (double) a.first.value /(double) a.first.weight; 
+			double r2 = (double) b.first.value /(double) b.first.weight;
+			return r1>r2;
+		}
+		
+		pair<double, vector<double>> fk(int W, Item arr[], int n){
+//			sort(arr, arr+n, comp);
+            vector<pair<Item, int>> ind;
+            for (int i = 0; i < n; i++) {
+                ind.push_back({arr[i], i}); // Store the item along with its index
             }
-            return final;
-        }
-    
+
+
+            sort(ind.begin(), ind.end(), comp);
+
+			
+			vector<double> included(n,0.0);
+			int cur=0;
+			double final=0.0; //final value
+			
+			for(int i=0; i<n;i++){
+				int idx = ind[i].second;
+				
+				if(cur + ind[i].first.weight <= W){
+					cur += ind[i].first.weight;
+					final += ind[i].first.value;
+					included[idx]=1;
+				}
+				else {
+					int rem = W-cur;  // remainder weight
+					final += (double) rem * (double) ind[i].first.value / (double) ind[i].first.weight;
+					included[idx]=(double)rem/(double)ind[i].first.weight;
+					break;
+				}
+			}
+			return {final, included};
+		}
 };
+
 int main() {
-   int n = 3, weight = 50;
-   Item arr[n] = { {100,20},{60,10},{120,30} };
-   S obj;
-   double ans = obj.fk(weight, arr, n);
-   cout << "The maximum value is " << setprecision(2) << fixed << ans;
+   int n = 3, weight = 60;
+   Item arr[n] = { {60,15},{100,30},{150,45} };
+   Solution obj;
+   
+   double ans = obj.fk(weight, arr, n).first;
+   vector<double> soln = obj.fk(weight, arr, n).second;cout << "The maximum value is " << ans;
+   
+   
+   cout<<"\nElements included are : ";
+   for(int i=0; i<n;i++){
+   	cout<<soln[i]<<" ";
+   }
    return 0;
 }
+
+
+
+/*
+    things to remember -> bool static comp, data structure -> pair<Item, int>  auxillary array that maintains index 
+    idx = pair.second    Item -> a.first.value
+    First ind structure array and index push
+    sort, included vector
+    for loop -> extract largest weight to profit, included[idx] = 1 to maintain index
+*/
